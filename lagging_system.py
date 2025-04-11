7061210224e8a0f010c7e4727eaf898cb88c13e4
TonyIrvan <map27603@email.vccs.edu>
2025-04-11 11:12:06 -0400

    updated lagging system, added continuous motor script and deleted motor control script to restsrt

--- lagging_system.py
+++ lagging_system.py
@@ -2,20 +2,21 @@ import RPi.GPIO as GPIO
 import config
 import time
 
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(config.HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(config.LAGGING_MOTOR_PIN, GPIO.OUT)
+    GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.HIGH)  # Start motor
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(config.HALL_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(config.LAGGING_MOTOR_PIN, GPIO.OUT)
-#GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.HIGH)  # Motor runs by default
+    def hall_callback(channel):
+        print("Magnet detected - pausing system")
+        GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.LOW)
+        time.sleep(config.LAGGING_DELAY)
+        GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.HIGH)
 
-def hall_sensor_callback(channel):
-    print("Magnet detected! Stopping motor.")
-    GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.LOW)
-    time.sleep(config.LAGGING_DELAY)
-    print("Resuming motor.")
-    GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.HIGH)
+    GPIO.add_event_detect(config.HALL_SENSOR_PIN, GPIO.FALLING,
+                          callback=hall_callback, bouncetime=200)
 
-GPIO.add_event_detect(config.HALL_SENSOR_PIN, GPIO.FALLING, callback=hall_sensor_callback, bouncetime=200)
 
 def cleanup():
-    GPIO.cleanup()
+    GPIO.output(config.LAGGING_MOTOR_PIN, GPIO.LOW)
